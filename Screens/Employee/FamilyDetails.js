3eb1585c70c9493e11c9c980c7c13f4a0bc91625
Ajay <misdeptho@gmail.com>
2024-05-22 18:13:53 -0400

    family screen complete

--- Screens/Employee/FamilyDetails.js
+++ Screens/Employee/FamilyDetails.js
@@ -4,12 +4,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import { Picker } from "@react-native-picker/picker";
 import Icon from 'react-native-vector-icons/FontAwesome'; // Import the icon library
-
 const FamilyDetails = ({ navigation }) => {
   const [token, setToken] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formChanged, setFormChanged] = useState(false);
   const [familyDetails, setFamilyDetails] = useState([
-    { relation: '', name: '', age: '', work: '', monthSalary: '', phoneNo: '' }
+    { FamilyId: '', relation: '', name: '', age: '', work: '', monthSalary: '', phoneNo: '' }
   ]);
   const [languages, setLanguages] = useState([]);
   const [languageSections, setLanguageSections] = useState([
@@ -17,7 +18,7 @@ const FamilyDetails = ({ navigation }) => {
   ]);
 
   const handleAddFamily = () => {
-    setFamilyDetails([...familyDetails, { relation: '', name: '', age: '', work: '', monthSalary: '', phoneNo: '' }]);
+    setFamilyDetails([...familyDetails, { FamilyId: '', relation: '', name: '', age: '', work: '', monthSalary: '', phoneNo: '' }]);
   };
 
   const handleDeleteFamily = (index) => {
@@ -33,6 +34,14 @@ const FamilyDetails = ({ navigation }) => {
     fetchLanguages();
     checkAuthentication();
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      fetchFamilyDetails();
+      fetchLanguageDetails();
+    }
+  }, [token]);
+  
 
   const checkAuthentication = async () => {
     try {
@@ -82,9 +91,20 @@ const FamilyDetails = ({ navigation }) => {
   };
 
   const handleSubmit = async () => {
-    console.log(token,"shddsfoj")
+    if (isSubmitting) {
+      return; // Prevent multiple submissions
+    }
+    setIsSubmitting(true);
+  
     try {
-      const familyResponses = await Promise.all(familyDetails.map(async (family) => {
+      // Separate the entries into new and existing
+      const newFamilies = familyDetails.filter(family => !family.FamilyId);
+      const existingFamilies = familyDetails.filter(family => family.FamilyId);
+      const newLanguages = languageSections.filter(language => !language.AppLanId);
+      const existingLanguages = languageSections.filter(language => language.AppLanId);
+  
+      // Handle new family entries
+      const newFamilyResponses = await Promise.all(newFamilies.map(async (family) => {
         return await axios.post(
           "http://10.0.2.2:3000/api/v1/fam/family",
           {
@@ -103,13 +123,39 @@ const FamilyDetails = ({ navigation }) => {
           }
         );
       }));
-
-      const familySuccess = familyResponses.every(response => response.data.success);
-
-      if (familySuccess) {
-        Alert.alert("Success", "Family details added successfully");
-
-        const languageResponses = await Promise.all(languageSections.map(async (language) => {
+  
+      const familySuccess = newFamilyResponses.every(response => response.data.success);
+  
+      // Handle existing family entries
+      const updateFamilyResponses = await Promise.all(existingFamilies.map(async (family) => {
+        return await axios.put(
+          "http://10.0.2.2:3000/api/v1/fam/updatefam",
+          {
+            FamilyId: family.FamilyId,
+            Relation: family.relation,
+            Name: family.name,
+            Age: family.age,
+            Work: family.work,
+            MonthSalary: family.monthSalary,
+            PhoneNo: family.phoneNo
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`
+            }
+          }
+        );
+      }));
+  
+      const updateFamilySuccess = updateFamilyResponses.every(response => response.data.success);
+  
+      if (familySuccess && updateFamilySuccess) {
+        const familyIds = newFamilyResponses.map(response => response.data.FamilyId);
+        await AsyncStorage.setItem('FamilyIds', JSON.stringify(familyIds));
+  
+        // Handle new language entries
+        const newLanguageResponses = await Promise.all(newLanguages.map(async (language) => {
           return await axios.post(
             "http://10.0.2.2:3000/api/v1/fam/postLng",
             {
@@ -126,21 +172,182 @@ const FamilyDetails = ({ navigation }) => {
             }
           );
         }));
-
-        const languageSuccess = languageResponses.every(response => response.data.success);
-
-        if (languageSuccess) {
-          Alert.alert("Success", "Language details added successfully");
-          // Navigate to next screen or perform any other action
+  
+        const languageSuccess = newLanguageResponses.every(response => response.data.success);
+  
+        // Handle existing language entries
+        const updateLanguageResponses = await Promise.all(existingLanguages.map(async (language) => {
+          return await axios.put(
+            "http://10.0.2.2:3000/api/v1/fam/updateLan",
+            {
+              AppLanId: language.AppLanId,
+              LanId: language.LanId,
+              LanSpeak: language.LanSpeak,
+              LanRead: language.LanRead,
+              LanWrite: language.LanWrite
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+              }
+            }
+          );
+        }));
+  
+        const updateLanguageSuccess = updateLanguageResponses.every(response => response.data.success);
+  
+        if (languageSuccess && updateLanguageSuccess) {
+          const addedLanguageIds = newLanguageResponses.map(response => response.data.AppLanId);
+          await AsyncStorage.setItem("addedLanguageIds", JSON.stringify(addedLanguageIds));
+  
+          Alert.alert("Success", "Family and Language details added and updated successfully");
         } else {
-          Alert.alert("Error", "Failed to add languageSections details");
+          Alert.alert("Error", "Failed to add or update language details");
         }
       } else {
-        Alert.alert("Error", "Failed to add family details");
+        Alert.alert("Error", "Failed to add or update family details");
       }
     } catch (error) {
       console.error("Error:", error.message);
       Alert.alert("Error", "Failed to add data");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+  
+
+  const fetchFamilyDetails = async () => {
+    try {
+      const response = await axios.get(
+        "http://10.0.2.2:3000/api/v1/fam/getFam",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+  
+      if (response.data.success) {
+        const fetchedFamily = response.data.data.map((family) => ({
+          FamilyId: family.FamilyId,
+          relation: family.Relation,
+          name: family.Name,
+          age: family.Age.toString(),
+          work: family.Work,
+          monthSalary: family.MonthSalary.toString(),
+          phoneNo: family.PhoneNo
+        }));
+        setFamilyDetails(fetchedFamily);
+      }
+    } catch (error) {
+      console.error("Error fetching FamilyDetails:", error.message);
+    }
+  };
+  
+  const handleUpdatefamily = async (index) => {
+    if (isSubmitting) {
+      return; // Prevent multiple submissions
+    }
+    setIsSubmitting(true);
+    try {
+      const family = familyDetails[index];
+      const response = await axios.put(
+        "http://10.0.2.2:3000/api/v1/fam/updatefam",
+        {
+          FamilyId: family.FamilyId, // Include FamilyId for update
+          Relation: family.relation,
+          Name: family.name,
+          Age: family.age,
+          Work: family.work,
+          MonthSalary: family.monthSalary,
+          PhoneNo: family.phoneNo
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.data.success) {
+        throw new Error(response.data.message || "Failed to update family");
+      }
+
+      Alert.alert("Success", "Family updated successfully");
+      setFormChanged(false); // Reset form change tracking
+    } catch (error) {
+      console.error("Error updating Family:", error.message);
+      Alert.alert("Error", "Failed to update Family");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const fetchLanguageDetails = async () => {
+    try {
+      const response = await axios.get(
+        "http://10.0.2.2:3000/api/v1/fam/getLan",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+  
+      if (response.data.success) {
+        const fetchedLanguage = response.data.data.map((language) => ({
+          AppLanId: language.AppLanId,
+          LanId: language.LanId,
+          LanSpeak: language.LanSpeak,
+          LanRead: language.LanRead,
+          LanWrite: language.LanWrite
+        }));
+        setLanguageSections(fetchedLanguage);
+      }
+    } catch (error) {
+      console.error("Error fetching LanguageDetails:", error.message);
+    }
+  };
+
+  const handleUpdateLanguage = async (index) => {
+    if (isSubmitting) {
+      return; // Prevent multiple submissions
+    }
+    setIsSubmitting(true);
+    try {
+      const language = languageSections[index];
+      const response = await axios.put(
+        "http://10.0.2.2:3000/api/v1/fam/updateLan",
+        {
+          AppLanId:language.AppLanId,
+          LanId: language.LanId,
+          LanSpeak: language.LanSpeak,
+          LanRead: language.LanRead,
+          LanWrite: language.LanWrite
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.data.success) {
+        throw new Error(response.data.message || "Failed to update Languages");
+      }
+
+      Alert.alert("Success", "Languages updated successfully");
+      setFormChanged(false); // Reset form change tracking
+    } catch (error) {
+      console.error("Error updating Languages:", error.message);
+      Alert.alert("Error", "Failed to update Languages");
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -149,7 +356,7 @@ const FamilyDetails = ({ navigation }) => {
       <Text style={styles.sectionTitle}>Family Details</Text>
       {familyDetails.map((family, index) => (
         <View key={index} style={styles.inputContainer}>
-           <Text style={styles.text}>select relation :</Text>
+          <Text style={styles.text}>Select relation :</Text>
           <Picker
             style={styles.input}
             selectedValue={family.relation}
@@ -163,35 +370,35 @@ const FamilyDetails = ({ navigation }) => {
             <Picker.Item label="Spouse" value="W" />
             <Picker.Item label="Children" value="C" />
           </Picker>
-          <Text style={styles.text}>name :</Text>
+          <Text style={styles.text}>Name :</Text>
           <TextInput
             style={styles.input}
             placeholder="name"
             value={family.name}
             onChangeText={(value) => handleInputChange(index, 'name', value)}
           />
-          <Text style={styles.text}>age :</Text>
+          <Text style={styles.text}>Age :</Text>
           <TextInput
             style={styles.input}
             placeholder="age"
             value={family.age}
             onChangeText={(value) => handleInputChange(index, 'age', value)}
           />
-          <Text style={styles.text}>work:</Text>
+          <Text style={styles.text}>Work:</Text>
           <TextInput
             style={styles.input}
             placeholder="work"
             value={family.work}
             onChangeText={(value) => handleInputChange(index, 'work', value)}
           />
-          <Text style={styles.text}>monthly salary :</Text>
+          <Text style={styles.text}>Monthly Salary :</Text>
           <TextInput
             style={styles.input}
             placeholder="monthSalary"
             value={family.monthSalary}
             onChangeText={(value) => handleInputChange(index, 'monthSalary', value)}
           />
-          <Text style={styles.text}>phone no :</Text>
+          <Text style={styles.text}>Phone No :</Text>
           <TextInput
             style={styles.input}
             placeholder="PhoneNO"
@@ -200,19 +407,22 @@ const FamilyDetails = ({ navigation }) => {
           />
 
           <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.addButton} onPress={() => handleUpdatefamily(index)}>
+              <Text style={styles.buttonText}>Update</Text>
+            </TouchableOpacity>
             <TouchableOpacity style={styles.deleteButton} onPress={() => handleDeleteFamily(index)}>
               <Text style={styles.buttonText}>Delete</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.addButton} onPress={handleAddFamily}>
-              <Text style={styles.buttonText}>Add Family</Text>
             </TouchableOpacity>
           </View>
         </View>
       ))}
+      <TouchableOpacity style={styles.addButton} onPress={handleAddFamily}>
+        <Text style={styles.buttonText}>Add Family</Text>
+      </TouchableOpacity>
       <Text style={styles.sectionTitle}>Language Details</Text>
       {languageSections.map((language, index) => (
         <View key={index} style={styles.inputContainer}>
-       <Text style={styles.text}>select lanaguage :</Text>
+          <Text style={styles.text}>Select language :</Text>
           <Picker
             selectedValue={language.LanId}
             onValueChange={(itemValue) => handleLanguageChange(index, 'LanId', itemValue)}
@@ -252,18 +462,20 @@ const FamilyDetails = ({ navigation }) => {
             <TouchableOpacity style={styles.deleteButton} onPress={() => handleDeleteLanguage(index)}>
               <Text style={styles.buttonText}>Delete</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.addButton} onPress={handleAddLanguage}>
-              <Text style={styles.buttonText}>Add Language</Text>
+            <TouchableOpacity style={styles.addButton} onPress={()=>handleUpdateLanguage(index)}>
+              <Text style={styles.buttonText}>Update</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.buttonsContainer}>
+        </View>
+      ))}
+       <TouchableOpacity style={styles.addButton} onPress={handleAddLanguage}>
+              <Text style={styles.buttonText}>Add Language</Text>
+            </TouchableOpacity>
+      <View style={styles.buttonsContainer}>
         <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
           <Text style={styles.buttonText}>Submit</Text>
         </TouchableOpacity>
       </View>
-        </View>
-      ))}
-      
     </ScrollView>
   );
 };
